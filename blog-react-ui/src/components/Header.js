415d3f10d5c25e6a5234b8c71b6dc3d8f5e0e431
mdarora <mdarora0001@gmail.com>
2021-09-20 12:02:27 +0530

    createdv header for home page

---
+++ blog-react-ui/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+    <header>
+        <h3>React & Node</h3>
+        <h1>Blog</h1>
+        <img src="https://images.pexels.com/photos/1576667/pexels-photo-1576667.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="Header backgroung" />
+    </header>
+    )
+}
+
+export default Header
---
+++ blog-react-ui/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Home = () => {
+    return (
+    <>
+        <Header></Header>
+        <h1>Homepage</h1>
+    </>
+    )
+}
+
+export default Home
