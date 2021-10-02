db831cda31346b5ec4c3c2c11b27d0c793820666
mdarora <mdarora0001@gmail.com>
2021-10-02 19:18:55 +0530

    using bcryptjs to hash passwords before saveing to database

--- blog-api/routes/auth.js
+++ blog-api/routes/auth.js
@@ -1,19 +1,32 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../db/models/User');
 
 router.post("/register", async (req, res) => {
-    const {username, email, password} = req.body;
     try {
+        const {username, email, password} = req.body;
         if(!username || !email || !password){
-            return res.status(401).json({error: "All fields are required"});
+            return res.status(401).json({error: "All fields are required."});
         }
-        const newUser = new User({username, email, password});
+
+        const findByUsername = await User.findOne({username});
+        if(findByUsername){
+            return res.status(401).json({error: "Username already exist."});
+        }
+
+        const findByEmail = await User.findOne({email});
+        if(findByEmail){
+            return res.status(401).json({error: "Email already exist."});
+        }
+
+        const hashedPass = await bcrypt.hash(password, 10);
+        const newUser = new User({username, email, password: hashedPass});
 
         await newUser.save();
-        return res.status(200).json({message: "User registered"});
+        return res.status(200).json({message: "User registered."});
         
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
         return res.status(500).json({error: "Something went Wrong!"});
     }
 });
