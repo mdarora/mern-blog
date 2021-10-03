0305d357737a4818b656b16bf46b2c0b20baec8a
mdarora <mdarora0001@gmail.com>
2021-10-03 18:45:55 +0530

    using cookie-parse and added posts routes

--- blog-api/index.js
+++ blog-api/index.js
@@ -8,9 +8,11 @@ app.get("/", (req, res)=> {
 });
 
 app.use(express.json());
+app.use(require('cookie-parser')());
 
 app.use("/api/auth", require('./routes/auth'));
 app.use("/api/users", require('./routes/users'));
+app.use("/api/posts", require('./routes/posts'));
 
 
 require('./db/dbConnection');
