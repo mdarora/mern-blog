33fb36e016a4e83b9dc72326701bb66ba9dd6584
mdarora <mdarora0001@gmail.com>
2021-10-11 14:04:53 +0530

    added routes for getting and deleting posts

--- blog-api/routes/posts.js
+++ blog-api/routes/posts.js
@@ -28,5 +28,38 @@ router.post("/", loginAuth, async (req, res) => {
     }
 });
 
+router.delete("/:id", loginAuth, async (req, res) => {
+    try {
+        const findPostById = await Post.findOne({_id: req.params.id});
+
+        if(!findPostById){
+            return res.status(402).json({error: "Invalid request."});
+        } else if (findPostById.userId !== req.id){
+            return res.status(401).json({error: "You can only delete your own post only!"});
+        }
+        await findPostById.delete();
+
+        res.status(200).json({message: "Post deleted."});
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({error: "Something went Wrong!"});
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const findPostById = await Post.findOne({_id: req.params.id});
+        if(!findPostById){
+            return res.status(404).json({error: "Post not found."});
+        }
+
+        res.status(200).json({message: "Post found", post: findPostById});
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({error: "Something went Wrong!"});
+    }
+});
 
 module.exports = router;
