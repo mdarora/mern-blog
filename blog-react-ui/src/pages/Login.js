0ad004b4ec470ed9ed878abd35c7aa81d19289d9
mdarora <mdarora0001@gmail.com>
2021-10-14 16:23:43 +0530

    connected login page to the login route of api

--- blog-react-ui/src/pages/Login.js
+++ blog-react-ui/src/pages/Login.js
@@ -1,17 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {useHistory} from "react-router-dom";
 
 const Login = () => {
+
+    const history = useHistory();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [resText, setResText] = useState("");
+
+    const userLogin = async (e) => {
+        e.preventDefault();
+        if(!username || !password) {
+            return setResText("All fields are required.");
+        }
+
+        try {
+            const res = await fetch("/api/auth/login", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({username, password})
+            });
+
+            const result = await res.json();
+
+            if(result.error){
+                return setResText(result.error);
+            } else if (result.message){
+                history.push("/");
+            }
+        } catch (error) {
+            setResText("Something went wrong !");
+        }
+    }
     return (
     <main className="login-page">
-            <form className="login-form">
+            <form onSubmit={userLogin} className="login-form">
                 <h2>Login</h2>
                 <div className="login-form-group">
-                    <label htmlFor="email">Email</label> <br />
-                    <input type="email" id="email" placeholder="Enter your email" autoComplete="off" />
+                    <label htmlFor="username">username</label> <br />
+                    <input type="username" id="username" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete="off" />
                 </div>
                 <div className="login-form-group">
                     <label htmlFor="password">Password</label> <br />
-                    <input type="password" placeholder="Enter your password" id="password" autoComplete="off" />
+                    <input type="password" placeholder="Enter your password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete="off" />
+                </div>
+                <div className="loginResText">
+                    <span className="text-danger">{resText}</span>
                 </div>
                 <div className="login-btn">
                     <button type="submit">Login</button>
