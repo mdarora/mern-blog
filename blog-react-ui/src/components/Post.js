12b585950dd1a5dedb777fc8aeb652c3bc1884d1
mdarora <mdarora0001@gmail.com>
2023-03-12 03:49:48 -0400

    getting all posts from api and showing on home page

--- blog-react-ui/src/components/Post.js
+++ blog-react-ui/src/components/Post.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const post = () => {
+const post = ({post}) => {
     return (
     <>
         <div className="post">
-            <div className="post-img">
-                <img src="https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="beautiful beach with a sunset" />
+            <Link to="/post">
+                <div className="post-img">
+                    <img src={post.image} alt={`image : ${post.title}`} />
+                </div>
+            </Link>
+            <div className="postBody">
+                <div className="post-cats">
+                    {post.categories.map((cat, i) => (
+                        <span key={i}>{cat}</span>
+                    ))}
+                </div>
+                <div className="post-title">
+                    <Link to="/post">
+                        <h3>{post.title}</h3>
+                    </Link>
+                </div>
+                <div className="post-time">{new Date(post.createdAt).toDateString()}</div>
+                <p className="post-desc">
+                    {post.desc}
+                </p>
             </div>
-            <div className="post-cats">
-                <span>Music</span>
-                <span>Life</span>
-                <span>Nature</span>
-            </div>
-            <div className="post-title">
-                <Link to="/post">
-                    <h3>Lorem ipsum dolor sit amet.</h3>
-                </Link>
-            </div>
-            <div className="post-time">1 hour ago</div>
-            <p className="post-desc">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat odio fugit illo perspiciatis culpa excepturi velit omnis adipisci, aperiam, placeat facere voluptatem, cum repudiandae nemo ullam repellendus perferendis suscipit maxime saepe ad praesentium porro error tempora! Recusandae maiores commodi aut nam voluptates aliquid eius animi.
-            </p>
         </div> 
     </>
     )
--- blog-react-ui/src/pages/Home.js
+++ blog-react-ui/src/pages/Home.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Post from '../components/Post'
 import Sidebar from '../components/Sidebar'
 
 const Home = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+      
+        const getPosts = async() =>{
+            const res = await fetch("/api/posts", {
+                method: "GET"
+            });
+            const result = await res.json();
+
+            if(result.message){
+                setPosts(result.posts);
+            }
+            console.log(result.posts);
+        }
+        getPosts();
+    }, []);
+    
     return (
     <>
         <Header></Header>
         <div className="home-wrapper">
             <div className="posts">
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                {posts.map((post, index) => (
+                    <Post key={index} post={post}/>
+                ))}
             </div>
             <Sidebar></Sidebar>
         </div>
