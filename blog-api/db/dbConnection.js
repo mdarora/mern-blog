30eb50bc30a4b6224b64686c7353ba64e724a43a
mdarora <mdarora0001@gmail.com>
2023-03-11 22:32:37 -0500

    Added mongoose parameters

--- blog-api/db/dbConnection.js
+++ blog-api/db/dbConnection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.DATABASE, {
+    // useCreateIndex : true,
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+})
 .then(()=>{
     console.log("Database connected.");
 }).catch((err)=> {
